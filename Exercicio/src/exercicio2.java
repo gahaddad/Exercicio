3e15e225c1a5297c3189aee7333ce8fc0d7bedbb
gabriel <gab-haddad@hotmail.com>
2022-08-18 16:11:20 -0300

    Exercício commit

---
+++ Exercicio/src/exercicio2.java
@@ -0,0 +1,10 @@
+public class exercicio2 {
+    public static void main(String[] args){
+        for(int i = 150; i <= 300; i++){
+            if (i >  0 && i < 150){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
